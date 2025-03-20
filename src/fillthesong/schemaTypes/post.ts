4b039a278c738b22efef6dc5b69e5968e8184b42
Aline <91509515+httpaline@users.noreply.github.com>
2025-03-20 17:10:30 -0300

    blog

---
+++ src/fillthesong/schemaTypes/post.ts
@@ -0,0 +1,43 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+        defineField({
+      name: 'conteudo',
+      title: 'Conteúdo',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+  },
+})
